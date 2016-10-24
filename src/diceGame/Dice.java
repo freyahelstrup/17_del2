59bc07b4ec911287e417d9cb707b3b3f2dc8a50c
Freya Helstrup <freyagh@me.com>
2016-10-24 15:58:56 +0200

    Test commit and push

--- src/diceGame/Dice.java
+++ src/diceGame/Dice.java
@@ -4,6 +4,6 @@ public class Dice {
 	public static void main(String[] args) {
 		System.out.println("Danny has comitted to this");
 		System.out.println("Joakim has also comitted to this");
-		
+		//Freya has committed to this
 	}
 }
