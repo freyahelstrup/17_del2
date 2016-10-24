50d622b4b6dcf4053652696fb4925aa1969ef8b8
joakimpoulsen <joakimpoulsen@joakims-mbp.clients.wireless.dtu.dk>
2016-10-24 15:50:27 +0200

    Testing commit.

--- src/diceGame/Dice.java
+++ src/diceGame/Dice.java
@@ -3,5 +3,7 @@ package diceGame;
 public class Dice {
 	public static void main(String[] args) {
 		System.out.println("Danny has comitted to this");
+		System.out.println("Joakim has also comitted to this");
+		
 	}
 }
