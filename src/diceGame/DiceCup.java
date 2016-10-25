72bf9ac65404e522b1440c09eb40368367eaaf03
Freya Helstrup <freyagh@me.com>
2016-10-25 16:43:29 +0200

    Update DiceCup.java
    Added new method getDiceSum().
    Added code for all methods.

--- src/diceGame/DiceCup.java
+++ src/diceGame/DiceCup.java
@@ -2,24 +2,37 @@ package diceGame;
 
 public class DiceCup {
 	private int[] values;
+	private int diceSides;
 	
 	public DiceCup(int diceSides, int diceAmount){
-		
+		values = new int[diceAmount];
+		this.diceSides = diceSides;	
+		this.setAllValuesRandom();
 	}
 	
 	public int getSingleValue(int place){
-		return 0;
+		return values[place];
 	}
 	
 	public void setSingleValue(int place, int newValue){
-		
+		values[place] = newValue;
 	}
 	
 	public void setAllValues(int[] values){
-		
+		this.values = values;
 	}
 
+	public int getDiceSum(){
+		int sum = 0;
+		for (int i = 0; i<values.length;i++){
+			sum += values[i];
+		}
+		return sum;
+	}
+	
 	public void setAllValuesRandom(){
-		
+		for (int i = 0; i < values.length; i++){
+			values[i] = ( (int) (Math.random()*diceSides)+1);
+		}
 	}
 }
