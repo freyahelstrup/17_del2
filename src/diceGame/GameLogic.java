166483794b02c18532ef515918f2d1e4d5f672af
Freya Helstrup <freyagh@me.com>
2016-10-25 17:01:14 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/freyahelstrup/17_del2
    
    Conflicts:
    	src/diceGame/GameLogic.java

--- src/diceGame/GameLogic.java
+++ src/diceGame/GameLogic.java
@@ -2,47 +2,52 @@ package diceGame;
 
 public class GameLogic {
 	private Player[] players;
+	private DiceCup dice;
 	private final int diceAmount;
 	private final int diceSides;
 	
 	public GameLogic(){
+		diceAmount = 2;		diceSides = 6;
+		dice = new DiceCup(diceSides,diceAmount);
+		
 		resetGame(2,1000);
+		
 		
 	}
 	
 	public void resetGame(int playerAmount, int startBalance){
 		players = new Player[playerAmount];
-		for (Player i : players){
-			i.setBalance(startBalance);
+		for (int i = 0; i < players.length; i++){
+			players[i] = new Player(i+1,startBalance);
 		}
 	}
 	
 	public Player getPlayer(int place){
-		
+		return players[place];
 	}
 	
 	public void setPlayer(int place, Player player){
-		
+		players[place] = player;
 	}
 	
 	public void playGame(){
 		
 	}
 	
-	private Player playTurn(Player player){
-		
-	}
-	
-	private void doScoreChange(Square square){
-		
-	}
-	
-	private boolean doExtraTurn(Square square){
-		
-	}
-	
-	private int[] declareWinner(){
-		
-	}
+//	private Player playTurn(Player player){
+//		
+//	}
+//	
+//	private void doScoreChange(Square square){
+//		
+//	}
+//	
+//	private boolean doExtraTurn(Square square){
+//		
+//	}
+//	
+//	private int[] declareWinner(){
+//		
+//	}
 	
 }
