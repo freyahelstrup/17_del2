d8e6fef442d200766427fd8d975c8de6ab55f03d
Freya Helstrup <freyagh@me.com>
2016-10-25 16:04:56 +0200

    Update and rename Dice.java to DiceCup.java

---
+++ src/diceGame/DiceCup.java
@@ -0,0 +1,25 @@
+package diceGame;
+
+public class DiceCup {
+	private int[] values;
+	
+	public DiceCup(int diceSides, int diceAmount){
+		
+	}
+	
+	public int getSingleValue(int place){
+		return 0;
+	}
+	
+	public void setSingleValue(int place, int newValue){
+		
+	}
+	
+	public void setAllValues(int[] values){
+		
+	}
+
+	public void setAllValuesRandom(){
+		
+	}
+}
