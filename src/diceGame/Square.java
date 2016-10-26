886780d9feb6b2a5edd687c2299ae9ab4676f7bd
Freya Helstrup <freyagh@me.com>
2016-10-26 13:45:23 +0200

    Added Board and Square classes

---
+++ src/diceGame/Square.java
@@ -0,0 +1,32 @@
+package diceGame;
+
+public class Square {
+	private int id;
+	private String name;
+	private char effect; //'t' for extra turn, '0' for no effect
+	private int scoreChange;
+	
+	public Square(int id, String name, char effect, int scoreChange){
+		this.id = id;
+		this.name = name;
+		this.effect = effect;
+		this.scoreChange = scoreChange;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public char getEffect(){
+		return effect;
+	}
+	
+	public int getScoreChange(){
+		return scoreChange;
+	}
+	
+}
