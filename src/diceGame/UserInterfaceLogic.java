4bacd3d98c0885f95ba97581d8ddf9377d4b5b48
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-10-28 10:25:12 +0200

    How to make fields, the musical

---
+++ src/diceGame/UserInterfaceLogic.java
@@ -0,0 +1,22 @@
+package diceGame;
+
+import java.awt.Color;
+
+import desktop_fields.Field;
+import desktop_fields.Street;
+import desktop_resources.GUI;
+
+public class UserInterfaceLogic {
+	
+	/* HOW TO MAKE FIELDS
+	public void go(){
+		Field f = new Street.Builder()
+			.setBgColor(new Color(255,0,0))
+			.setTitle("Hej")
+			.build();
+		
+		
+		GUI.create(new Field[]{f});
+	}
+	*/
+}
