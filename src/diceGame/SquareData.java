054e5ad497fb68fa08dec5544a6b320f4fbc9ef0
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-10-28 09:02:32 +0200

    Navne kommer fra Messages
    Har sat square navne til at svare til navnene i Messages
    Derudover har jeg også indsat kommentarer der gør det lettere at
    sammenligne de forskellige klasser efter deres nummer i arrayet.

--- src/diceGame/SquareData.java
+++ src/diceGame/SquareData.java
@@ -2,71 +2,57 @@ package diceGame;
 
 public class SquareData {
 
-	private String[] names = {
-			
-			"Tower",
-			"Crater",
-			"Palace gates",
-			"Cold Desert",
-			"Walled city",
-			"Monastery",
-			"Black cave",
-			"Huts in the mountain",
-			"The Werewall",
-			"The pit",
-			"Goldmine"
-			
-	};
-	
+	private String[] names = Messages.getSquareNames(); 
+	//To make translation easier, the names must come from the Messages class
 	
 	private char[] effects = {
 			
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			1,
-			0,
-			0
+			' ',	//0
+			' ',	//1
+			' ',	//2
+			' ',	//3
+			' ',	//4
+			' ',	//5
+			' ',	//6
+			' ',	//7
+			't',	//8
+			' ',	//9
+			' '		//10
 			
 	};
 	
 
 	private int[] ScoreChanges = {
 			
-			250,
-			-100,
-			100,
-			-20,
-			180,
-			0,
-			-70,
-			60,
-			-80,
-			-50,
-			650,
+			250,	//0
+			-100,	//1
+			100,	//2
+			-20,	//3
+			180,	//4
+			0,		//5
+			-70,	//6
+			60,		//7
+			-80,	//8
+			-50,	//9
+			650,	//10
 			
 	};
 	
-	public String getName(int x){
+	public String getName(int place){
 	
-		return names[x];
+		return names[place];
 		
 	}
 	
-	public char getEffect(int x){
+	public char getEffect(int place){
 		
-		return effects[x];
+		return effects[place];
 		
 	}
 	
-	public int getScoreChange(int x){
+	public int getScoreChange(int place){
 		
-		return ScoreChanges[x];
+		return ScoreChanges[place];
 		
 	}
 
