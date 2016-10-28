180a7c07757e4d6ae0cde6d7c6d9426c1eb3b9e8
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-10-28 08:26:09 +0200

    Udkommenteret ID
    Der er ikke nogen id i contructoren ifølge vores nuværende diagrammer.
    Har ændret board så den passer.

--- src/diceGame/Board.java
+++ src/diceGame/Board.java
@@ -6,17 +6,17 @@ public class Board {
 	public Board(){
 		squares = new Square[11];
 		
-		squares[0] = new Square(2,"Tower",'0',250);
-		squares[1] = new Square(3,"Crater",'0',-100);
-		squares[2] = new Square(4,"Palace Gates",'0',100);
-		squares[3] = new Square(5,"Cold Desert",'0',-20);
-		squares[4] = new Square(6,"Walled City",'0',180);
-		squares[5] = new Square(7,"Monastery",'0',0);
-		squares[6] = new Square(8,"Black Cave",'0',-70);
-		squares[7] = new Square(9,"Huts in the mountain",'0',60);
-		squares[8] = new Square(10,"The Werewall (werewolf-wall)",'t',-80);
-		squares[9] = new Square(11,"The pit",'0',-50);
-		squares[10] = new Square(12,"Goldmine",'0',650);
+		squares[0] = new Square("Tower",'0',250);
+		squares[1] = new Square("Crater",'0',-100);
+		squares[2] = new Square("Palace Gates",'0',100);
+		squares[3] = new Square("Cold Desert",'0',-20);
+		squares[4] = new Square("Walled City",'0',180);
+		squares[5] = new Square("Monastery",'0',0);
+		squares[6] = new Square("Black Cave",'0',-70);
+		squares[7] = new Square("Huts in the mountain",'0',60);
+		squares[8] = new Square("The Werewall (werewolf-wall)",'t',-80);
+		squares[9] = new Square("The pit",'0',-50);
+		squares[10] = new Square("Goldmine",'0',650);
 		
 	}
 
--- src/diceGame/Square.java
+++ src/diceGame/Square.java
@@ -1,21 +1,22 @@
 package diceGame;
 
 public class Square {
-	private int id;
+	//private int id; 
 	private String name;
 	private char effect; //'t' for extra turn, '0' for no effect
 	private int scoreChange;
 	
-	public Square(int id, String name, char effect, int scoreChange){
-		this.id = id;
+	public Square(/*int id,*/ String name, char effect, int scoreChange){
+		//this.id = id;
 		this.name = name;
 		this.effect = effect;
 		this.scoreChange = scoreChange;
 	}
 	
+	/*
 	public int getId(){
 		return id;
-	}
+	}*/
 	
 	public String getName(){
 		return name;
