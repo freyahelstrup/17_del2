9217ccc0afa4c0518a416bcc3f60354504023277
Gustav Petersen <Rangewaver@gmail.com>
2016-10-31 15:33:30 +0100

    Added comments

--- src/diceGame/SquareData.java
+++ src/diceGame/SquareData.java
@@ -6,6 +6,7 @@ public class SquareData {
 	//To make translation easier, the names must come from the Messages class
 	
 	private static char[] effects = {
+	//Array for tiles which gives an extra turn - ' ' for no extra turn ; t for extra turn
 			
 			' ',	//0
 			' ',	//1
@@ -23,6 +24,7 @@ public class SquareData {
 	
 
 	private static int[] scoreChanges = {
+	//Array for Balance change values for when a player steps on a tile
 			
 			250,	//0
 			-100,	//1
@@ -38,18 +40,22 @@ public class SquareData {
 			
 	};
 	
+	
+	//Names are stores in Messages class
 	public static String getName(int place){
 	
 		return names[place];
 		
 	}
 	
+	//Method to check if tile gives an extra turn
 	public static char getEffect(int place){
 		
 		return effects[place];
 		
 	}
 	
+	//Method to get balance change values
 	public static int getScoreChange(int place){
 		
 		return scoreChanges[place];
