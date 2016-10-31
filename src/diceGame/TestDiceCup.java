767da0f0dd99b82d74ecd8915c6c5b1c897e3380
Freya Helstrup <freyagh@me.com>
2016-10-31 14:04:14 +0100

    TestDiceCup improved.

--- src/diceGame/TestDiceCup.java
+++ src/diceGame/TestDiceCup.java
@@ -8,34 +8,30 @@ public class TestDiceCup {
 
 	@Test
 	public void test() {
-		
+
 		int[] Runs = new int[30000];
-		
+
 		Boolean FailOnce = false;
 		int DiceSides = 6;
-		
+
 		DiceCup Cup = new DiceCup(DiceSides,1);
-		
-			for (int i = 0; i <= 30000; i++){
-			
+
+		for (int i = 0; i < 30000; i++){
+
+			Cup.setAllValuesRandom();
 			Runs[i] = Cup.getDiceSum();
 			
-			}
-			
-				for (int n = 0; n <= 30000; n++){
-					
-					if (Runs[n] <= 0 || Runs[n] > DiceSides ){
-						
-						FailOnce = true;
-						
-				} 
-					
-			}
-		
-		assertEquals(FailOnce, false);
+			if (Runs[i] <= 0 || Runs[i] > DiceSides ){
 				
+				FailOnce = true;
+			
+			}
+		}
+
+		assertEquals(FailOnce, false);
+
 	}
-		
+
 }
 
 
