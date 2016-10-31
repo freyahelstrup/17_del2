2e79730119f971b84c6bcaaa8d5b04fb7bf6c13e
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-10-31 14:45:47 +0100

    New package for tests

---
+++ src/diceGameTest/TestGameLogic.java
@@ -0,0 +1,15 @@
+package diceGameTest;
+
+import static org.junit.Assert.*;
+import diceGame.*;
+
+import org.junit.Test;
+
+public class TestGameLogic {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
