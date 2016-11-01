03ee2bf933bf2c4411febe5f660ab7b1306f454b
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2016-11-01 14:04:20 +0100

    Added test case 21

--- src/diceGameTest/TestGameLogic.java
+++ src/diceGameTest/TestGameLogic.java
@@ -8,8 +8,30 @@ import org.junit.Test;
 public class TestGameLogic {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCase21() {
+		int [] results = {12, 3};
+		GameLogic game = new GameLogic(true, results);
+		
+		game.resetGame(2, 2900);
+		game.playGame();
+		
+		Player[] players = game.getPlayers();
+		
+		assertEquals(players[0].getBalance(), 3550);
+		assertEquals(players[1].getBalance(), 2800);
+		
+	}
+	
+	public void testCase22(){
+		fail("not yet implemented");
+	}
+	
+	public void testCase23(){
+		fail("not yet implemented");
+	}
+	
+	public void testCase24(){
+		fail("not yet implemented");
 	}
 
 }
